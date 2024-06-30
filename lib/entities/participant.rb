2ef62b8607cd2adabd90649f2ccdd1c00879892c
di0nixs <di0niixs@gmail.com>
2024-06-30 18:03:27 +0300

    Add instance variables: bank, cards and sum

--- lib/entities/participant.rb
+++ lib/entities/participant.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Participant
+  include Constants
   include Validation
 
+  attr_accessor :bank, :cards, :sum
   attr_reader :name
 
-  def initialize; end
+  def initialize
+    @bank = INITIAL_BANK
+    @cards = []
+    @sum = INITIAL_SUM
+  end
 end
