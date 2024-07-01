aa5311d1f9d4ed83fa4a8f626c8871ff0ba38695
di0nixs <di0niixs@gmail.com>
2024-07-02 00:13:14 +0300

    Update Game class: add show game and main loop

--- lib/entities/game.rb
+++ lib/entities/game.rb
@@ -4,12 +4,13 @@ class Game
   include Constants
   include InputHandler
 
-  attr_accessor :player, :bank
+  attr_accessor :player, :bank, :display
   attr_reader :dealer, :deck
 
   def self.initialize_game
     game = new
     game.player = Player.new { |p| p.name = user_input('Введите ваше имя') }
+    Display.welcome(game.player.name)
     game
   rescue StandardError => e
     puts e.message
@@ -19,28 +20,50 @@ class Game
   def initialize
     @bank = INITIAL_BANK
     @deck = Deck.new
-    @display = Display.new
     @dealer = Dealer.new
     @player = nil
+    @display = nil
   end
 
   def start_game
     deal_cards(2)
     make_auto_bet
-    self
+
+    loop do
+      show_game
+      break if player.cards.size == 3 && dealer.cards.size == 3
+
+      begin
+        input = self.class.user_input(MAKE_MOVE).to_i
+        player.make_move(input, deck)
+        dealer.make_move(deck)
+      rescue StandardError => e
+        puts e.message
+        retry
+      end
+    end
   end
 
   private
 
+  def show_game
+    self.display = Display.new(self)
+    display&.show_game
+  end
+
   def deal_cards(counts = 1)
+    puts 'Раздача карт...'
     players.each { |player| deck.deliver_card(player, counts) }
+    sleep 1
   end
 
   def make_auto_bet
+    puts 'Автоматическая ставка в банк игры...'
     players.each do |player|
       player.bank -= 10
       self.bank += 10
     end
+    sleep 1
   end
 
   def players
