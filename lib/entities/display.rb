59e7f04f3e4d9f7e7f183984936e1a02b8efdd2f
di0nixs <di0niixs@gmail.com>
2024-07-02 00:25:54 +0300

    Add Display class(draft version)

--- lib/entities/display.rb
+++ lib/entities/display.rb
@@ -1,6 +1,40 @@
 # frozen_string_literal: true
 
 class Display
+  attr_reader :player, :dealer
 
-  def show_game; end
+  def self.welcome(name)
+    puts "Добро пожаловать в игру #{name}!"
+  end
+
+  def initialize(game)
+    @player = game.player
+    @dealer = game.dealer
+  end
+
+  def show_game
+    puts "Карты игрока #{dealer.name}: #{dealer_cards}"
+    # puts "\tСумма очков игрока #{dealer.name}: #{dealer.score}"
+    puts "Банк игрока #{dealer.name}: #{dealer.bank}"
+
+    puts "Ваши карты: #{player.cards.join(' ')}"
+    puts "Ваша сумма очков: #{player.score}"
+    puts "Ваш банк: #{player.bank}"
+    # puts yield if block_given?
+  end
+
+  # def display_winner
+  #
+  # end
+
+  # def who_win?(player)
+  # end
+
+  private
+
+  def dealer_cards
+    dealer.cards.map! { '*' }.join(' ')
+
+    # dealer.cards.join(', ')
+  end
 end
