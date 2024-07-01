9ada7ca76bd55fa8ff2cc52283f2c0bf318424ec
di0nixs <di0niixs@gmail.com>
2024-07-01 15:10:55 +0300

    Add start_game method and private methods: deal_cards, players and make_auto_bet

--- lib/entities/game.rb
+++ lib/entities/game.rb
@@ -4,7 +4,8 @@ class Game
   include Constants
   include InputHandler
 
-  attr_accessor :player
+  attr_accessor :player, :bank
+  attr_reader :dealer, :deck
 
   def self.initialize_game
     game = new
@@ -17,7 +18,32 @@ class Game
 
   def initialize
     @bank = INITIAL_BANK
+    @deck = Deck.new
+    @display = Display.new
     @dealer = Dealer.new
     @player = nil
+  end
+
+  def start_game
+    deal_cards(2)
+    make_auto_bet
+    self
+  end
+
+  private
+
+  def deal_cards(counts = 1)
+    players.each { |player| deck.deliver_card(player, counts) }
+  end
+
+  def make_auto_bet
+    players.each do |player|
+      player.bank -= 10
+      self.bank += 10
+    end
+  end
+
+  def players
+    [dealer, player]
   end
 end
