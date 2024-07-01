12f55e231d1f7fd93a5bec53b340deb63e18e226
di0nixs <di0niixs@gmail.com>
2024-07-01 15:08:52 +0300

    Add deliver_card method

--- lib/entities/deck.rb
+++ lib/entities/deck.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 class Deck
+  include Constants
+
+  attr_accessor :cards
 
   def initialize
-    @cards = nil
+    @cards = CARDS
   end
 
-  def deliver_card(participant, counts); end
+  def deliver_card(participant, counts)
+    participant.cards << add_card!(counts)
+  end
+
+  private
+
+  def add_card!(counts = 1)
+    cards.shuffle!.pop(counts)
+  end
 end
