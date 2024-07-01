fc1fb28e4ba8f92071edd8518b1e06fd7b4cdb18
di0nixs <di0niixs@gmail.com>
2024-07-02 00:23:16 +0300

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'lib/modules/constants'
+require_relative 'lib/modules/input_handler'
+require_relative 'lib/modules/validation'
+require_relative 'lib/entities/participant'
+require_relative 'lib/entities/player'
+require_relative 'lib/entities/dealer'
+require_relative 'lib/entities/deck'
+require_relative 'lib/entities/display'
+require_relative 'lib/entities/game'
+
+Game.initialize_game.start_game
