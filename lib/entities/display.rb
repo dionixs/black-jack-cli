53984ea8fe5f5371f9d08719915fae02dd0fb8ea
di0nixs <di0niixs@gmail.com>
2024-07-02 23:46:34 +0300

    Update Display class

--- lib/entities/display.rb
+++ lib/entities/display.rb
@@ -1,40 +1,57 @@
 # frozen_string_literal: true
 
 class Display
-  attr_reader :player, :dealer
+  attr_reader :game, :player, :dealer
 
   def self.welcome(name)
     puts "Добро пожаловать в игру #{name}!"
   end
 
   def initialize(game)
+    @game = game
     @player = game.player
     @dealer = game.dealer
   end
 
   def show_game
-    puts "Карты игрока #{dealer.name}: #{dealer_cards}"
-    # puts "\tСумма очков игрока #{dealer.name}: #{dealer.score}"
-    puts "Банк игрока #{dealer.name}: #{dealer.bank}"
-
-    puts "Ваши карты: #{player.cards.join(' ')}"
-    puts "Ваша сумма очков: #{player.score}"
-    puts "Ваш банк: #{player.bank}"
-    # puts yield if block_given?
+    dealer_info
+    player_info
+    display_winner
   end
-
-  # def display_winner
-  #
-  # end
-
-  # def who_win?(player)
-  # end
 
   private
 
-  def dealer_cards
-    dealer.cards.map! { '*' }.join(' ')
+  def display_winner
+    return puts 'Ничья' if game.draw?
 
-    # dealer.cards.join(', ')
+    puts "Победитель игры: #{game.winner.name}"
+  end
+
+  def dealer_info
+    puts "Карты игрока #{dealer.name}: #{dealer_cards}"
+    puts dealer_score if game.game_end?
+    puts "Банк игрока #{dealer.name}: #{dealer.bank}"
+  end
+
+  def player_info
+    puts "Ваши карты: #{player_cards}"
+    puts "Ваша сумма очков: #{player.score}"
+    puts "Ваш банк: #{player.bank}"
+  end
+
+  def dealer_cards
+    if game.game_end?
+      dealer.cards.join(', ')
+    else
+      dealer.cards.map { '*' }.join(' ')
+    end
+  end
+
+  def dealer_score
+    "Сумма очков игрока #{dealer.name}: #{dealer.score}"
+  end
+
+  def player_cards
+    player.cards.join(' ')
   end
 end
