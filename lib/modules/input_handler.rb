f0f3de80b4f0ca6ef5ac2adae49466f3373241fa
di0nixs <di0niixs@gmail.com>
2024-07-03 13:14:54 +0300

    Add #make_move_input method in InputHandler module

--- lib/modules/input_handler.rb
+++ lib/modules/input_handler.rb
@@ -3,12 +3,28 @@
 module InputHandler
   def self.included(base)
     base.extend ClassMethods
+    base.include InstanceMethods
   end
 
   module ClassMethods
     def user_input(message)
       print "#{message}: "
       gets&.strip
+    end
+  end
+
+  module InstanceMethods
+    include Constants
+
+    def make_move_input(player)
+      message = message(player)
+      self.class.user_input(message).to_i
+    end
+
+    private
+
+    def message(player)
+      player.three_cards? ? MAKE_MOVE_WITHOUT_ADD_CARD : MAKE_MOVE
     end
   end
 end
