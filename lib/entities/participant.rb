4bd4a7edd947f77d894221658c33f19b9a47087f
di0nixs <di0niixs@gmail.com>
2024-07-03 16:38:18 +0300

    Add .winner and #lose, #win methods

--- lib/entities/participant.rb
+++ lib/entities/participant.rb
@@ -7,6 +7,24 @@ class Participant
   attr_accessor :bank, :cards, :score
   attr_reader :name
 
+  class << self
+    def all
+      ObjectSpace.each_object(self).to_a
+    end
+
+    def winner
+      participants = all
+      player = participants.find { |p| p.is_a?(Player) }
+      dealer = participants.find { |p| p.is_a?(Dealer) }
+
+      return dealer if player.lose?
+
+      return player if dealer.lose?
+
+      player.score > dealer.score ? player : dealer
+    end
+  end
+
   def initialize
     @bank = INITIAL_BANK
     @cards = []
@@ -14,6 +32,14 @@ class Participant
   end
 
   def make_move(game); end
+
+  def lose?
+    score > 21
+  end
+
+  def win(bank)
+    self.bank += bank
+  end
 
   def three_cards?
     cards.size == 3
